0135286b7c5fd80f9d45ee24fe6515727b977f04
RadoslawGorniak <radekg08@wp.pl>
2019-10-23 08:36:32 +0200

    Initial commit

---
+++ src/main/java/com/gorniak/allisjava/incomes/InComesApplication.java
@@ -0,0 +1,13 @@
+package com.gorniak.allisjava.incomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InComesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InComesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gorniak/allisjava/incomes/InComesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorniak.allisjava.incomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InComesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
